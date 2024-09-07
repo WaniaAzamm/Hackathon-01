8c9734daa96d62b0e4020d4ebc390508bf252b78
Wania Azam <159264679+WaniaAzamm@users.noreply.github.com>
2024-09-07 20:30:19 +0500

    Add files via upload

---
+++ hackathon/main.ts
@@ -0,0 +1,13 @@
+
+const toggleButton = document.getElementById('toggleSkills') as HTMLButtonElement;
+const skillsSection = document.getElementById('skills') as HTMLDivElement;
+
+toggleButton.addEventListener('click', () => {
+  if (skillsSection.style.display === 'none') {
+    skillsSection.style.display = 'block';
+    toggleButton.textContent = 'Hide Skills';
+  } else {
+    skillsSection.style.display = 'none';
+    toggleButton.textContent = 'Show Skills';
+  }
+});
