56c57a6bf4fa5d2a3d3ea87b4b415c0352155084
Wania Azam <159264679+WaniaAzamm@users.noreply.github.com>
2024-09-18 20:00:31 +0500

    Update main.js

--- milestone-03/main.js
+++ milestone-03/main.js
@@ -21,6 +21,7 @@ var _a;
         var resumeHTML = "\n            <div class=\"resume-header\">\n                <img src=\"".concat(profilePicUrl, "\" alt=\"Profile Picture\">\n                <h1>").concat(name, "</h1>\n                <p>Email: ").concat(email, "</p>\n                <p>Contact: ").concat(contact, "</p>\n                <p>Date of Birth: ").concat(dob, "</p>\n            </div>\n\n            <div class=\"resume-section\">\n                <h3>Education</h3>\n                <p>").concat(education, "</p>\n            </div>\n\n            <div class=\"resume-section\">\n                <h3>Work Experience</h3>\n                <p><strong>Position:</strong> ").concat(position, "</p>\n                <p><strong>Company:</strong> ").concat(company, "</p>\n                <p><strong>Duration:</strong> ").concat(duration, "</p>\n            </div>\n\n            <div class=\"resume-section\">\n                <h3>Skills</h3>\n                <ul>\n                    ").concat(skills.map(function (skill) { return "<li>".concat(skill.trim(), "</li>"); }).join(''), "\n                </ul>\n            </div>\n        ");
         resume.innerHTML = resumeHTML;
     };
+    
     if (profilePicture) {
         reader.readAsDataURL(profilePicture);
     }
